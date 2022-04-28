a1f6eea4a57e332a1c5efbdbe2f99242ee2711e7
infuq <infuq217@outlook.com>
2022-04-28 15:06:32 +0800

    ...

--- infuq-t/src/main/java/com/infuq/consumer/QueryBookServiceImpl.java
+++ infuq-t/src/main/java/com/infuq/consumer/QueryBookServiceImpl.java
@@ -3,21 +3,33 @@ package com.infuq.consumer;
 
 import com.infuq.provider.BookFacade;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 @Service
-public class QueryBookServiceImpl implements QueryBookService {
+public class QueryBookServiceImpl implements QueryBookService, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Reference(version = "1.0", group = "infuq-t", retries = 0, timeout = 50000)
-    private BookFacade facade;
+    private BookFacade bookfacade;
 
 
     @Override
     public String query(String bookName) {
 
-        return facade.query(bookName);
+        BookFacade service = applicationContext.getBean(BookFacade.class);
+
+
+        return bookfacade.query(bookName);
 
     }
 
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
